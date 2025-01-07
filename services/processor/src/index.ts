07eb08a78a77461cf113b432c8a17beaa2057639
Mark Omarov <15357910+mark-omarov@users.noreply.github.com>
2025-01-07 10:21:25 +0900

    refactor: better processor error handling

--- services/processor/src/index.ts
+++ services/processor/src/index.ts
@@ -12,38 +12,50 @@ import {
   updateJobStatus,
 } from './domain/thumbnail.service.js';
 
-const minioClient = createMinioClient({
-  endPoint: env.MINIO_ENDPOINT,
-  port: env.MINIO_PORT,
-  accessKey: env.MINIO_ACCESS_KEY,
-  secretKey: env.MINIO_SECRET_KEY,
-  useSSL: env.MINIO_SSL,
-});
+async function main() {
+  const minioClient = createMinioClient({
+    endPoint: env.MINIO_ENDPOINT,
+    port: env.MINIO_PORT,
+    accessKey: env.MINIO_ACCESS_KEY,
+    secretKey: env.MINIO_SECRET_KEY,
+    useSSL: env.MINIO_SSL,
+  });
 
-const storage = createMinioStorageAdapter(minioClient, env.MINIO_BUCKET);
-const jobRepository = createPgJobRepositoryAdapter();
-const jobQueue = createBullmqJobQueueAdapter({
-  host: env.REDIS_HOST,
-  port: env.REDIS_PORT,
-});
+  const storage = createMinioStorageAdapter(minioClient, env.MINIO_BUCKET);
+  const jobRepository = createPgJobRepositoryAdapter();
+  const jobQueue = createBullmqJobQueueAdapter({
+    host: env.REDIS_HOST,
+    port: env.REDIS_PORT,
+  });
 
-jobQueue.createQueueWorker(async (job) => {
-  try {
-    switch (job.name) {
-      case bullmqJobs.createThumbnail:
-        const { jobId, originalImagePath } = job.data;
-        await processThumbnail(jobId, originalImagePath, {
-          storage,
-          jobRepository,
-        });
-        return;
-      default:
-        console.error(`Unknown job name: ${job.name}`);
+  jobQueue.createQueueWorker(async (job) => {
+    try {
+      switch (job.name) {
+        case bullmqJobs.createThumbnail:
+          const { jobId, originalImagePath } = job.data;
+          await processThumbnail(jobId, originalImagePath, {
+            storage,
+            jobRepository,
+          });
+          return;
+        default:
+          console.error(`Unknown job name: ${job.name}`);
+      }
+    } catch (error) {
+      console.error('Error processing job:', error);
+      await updateJobStatus(job.data.jobId, 'failed', undefined, jobRepository);
     }
-  } catch (error) {
-    console.error('Error processing job:', error);
-    await updateJobStatus(job.data.jobId, 'failed', undefined, jobRepository);
-  }
+  });
+}
+
+main()
+  .then(() => console.log('Processor service started'))
+  .catch((err) => console.error(err));
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
 });
 
-console.log('Processor services started.');
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+});
